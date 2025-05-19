fffed4e0ab3d89d88c67b4786ad02b685b3d9845
spotfurnish12 <spotfurnish@gmail.com>
2025-05-19 17:30:52 +0530

    Update endpoint.js

--- client/src/endpoint.js
+++ client/src/endpoint.js
@@ -1,3 +1,3 @@
-//export const API_URL ='https://spotfurnishrental-g16b.vercel.app';
+export const API_URL ='https://spotfurnishrental-g16b.vercel.app';
 //export const API_URL = 'http://localhost:5000'
-export const API_URL = 'https://furnishrent.onrender.com'
+//export const API_URL = 'https://furnishrent.onrender.com'
